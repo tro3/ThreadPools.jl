1296d22697cbf17da8d709d509011da13c2e9be4
Ian Butterworth <i.r.butterworth@gmail.com>
2022-02-01 13:31:58 -0500

    limit thread tests to possible range

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 
 let cmd = `$(Base.julia_cmd()) --depwarn=error --startup-file=no runtests_exec.jl`
-    for test_nthreads in (1, 2, 4) # run once to try single-threaded mode, then try a couple times to trigger bad races
+    for test_nthreads in sort(collect(Set((1, 2, Threads.nthreads())))) # run once to try single-threaded mode, then try a couple times to trigger bad races
         new_env = copy(ENV)
         new_env["JULIA_NUM_THREADS"] = string(test_nthreads)
         println("\n# Threads = $test_nthreads")
